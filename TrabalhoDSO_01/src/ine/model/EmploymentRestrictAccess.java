ddde59d9c9c304991c250fa84d4535dd3241fb9b
sadi <robalo_pescador@hotmail.com>
2017-10-08 12:59:41 -0300

    Nada relevante feito, estou enviando esse commit apenas por ter me esquecido de, no último commit, subir também a nova classe criada

---
+++ TrabalhoDSO_01/src/ine/model/EmploymentRestrictAccess.java
@@ -0,0 +1,37 @@
+package ine.model;
+
+import java.util.ArrayList;
+
+import ine.controller.EmploymentCtrl;
+
+public class EmploymentRestrictAccess extends Employment {
+	private ArrayList<Horary> horarys;
+
+	public EmploymentRestrictAccess(int code, String name,
+			Privileges privilege, EmploymentCtrl employmentCtrl) {
+		super(code, name, privilege, employmentCtrl);
+		horarys = new ArrayList();
+		
+	}
+	
+	public void addHorary(Horary horary) {
+		horarys.add(horary);
+	}
+	
+	public void addHoraryByIndex(Horary horary, int index) {
+		horarys.set(index, horary);
+	}
+	
+	public void delHorary(int index) {
+		horarys.remove(index);
+	}
+	
+	public ArrayList<Horary> getHorarys() {
+		return horarys;
+	}
+	
+	public Horary getHorary(int index) {
+		return horarys.get(index);
+	}
+
+}
