fd12b252f110edfd86a5e0b98feafead6057c4c1
sadi <robalo_pescador@hotmail.com>
2017-10-11 08:59:01 -0300

    Tá rodando, filha da puta!

--- TrabalhoDSO_01/src/ine/view/AccessScreen.java
+++ TrabalhoDSO_01/src/ine/view/AccessScreen.java
@@ -33,17 +33,27 @@ public class AccessScreen {
 	}
 
 	public void listDeniedAccessByNumRegistration(Access a) {
-		System.out.println("Number of Registration - " + a.getNumRegistration());
-		System.out.println("Date of Access - " + a.getDate().toString());
-		System.out.println("Hour of Access - " + a.getHour().toString());
-		System.out.println("Reason for denial of access - " + a.getReason());
+		try {
+			System.out.println("Number of Registration - " + a.getNumRegistration());
+			System.out.println("Date of Access - " + accessCtrl.dateToStringDate(a.getDate()));
+			System.out.println("Hour of Access - " + accessCtrl.dateToStringHour(a.getHour()));
+			System.out.println("Reason for denial of access - " + a.getReason());
+		} catch(ParseException e) {
+			System.out.println("Deu ruim");
+		}
+		
 	}
 
 	public void listDeniedAccessByReason(Access a) {
-		System.out.println("Number of Registration - " + a.getNumRegistration());
-		System.out.println("Date of Access - " + a.getDate().toString());
-		System.out.println("Hour of Access - " + a.getHour().toString());
-		System.out.println("Reason for denial of access - " + a.getReason());
+		try {
+			System.out.println("Number of Registration - " + a.getNumRegistration());
+			System.out.println("Date of Access - " + accessCtrl.dateToStringDate(a.getDate()));
+			System.out.println("Hour of Access - " + accessCtrl.dateToStringHour(a.getHour()));
+			System.out.println("Reason for denial of access - " + a.getReason());
+		} catch(ParseException e) {
+			System.out.println("Deu ruim");
+		}
+		
 	}
 	
 }
