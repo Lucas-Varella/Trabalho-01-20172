c934543d1db0d8482af1abf24605075a3e36a243
MarcosLaydner <30905990+MarcosLaydner@users.noreply.github.com>
2017-11-01 16:34:31 -0200

    MainScreen graphic prototype

--- TrabalhoDSO_01/src/br/ufsc/ine5605/view/MainScreenI.java
+++ TrabalhoDSO_01/src/br/ufsc/ine5605/view/MainScreenI.java
@@ -1,5 +1,29 @@
 package br.ufsc.ine5605.view;
 
-public class MainScreenI {
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MainScreenI extends JFrame{
+	private JLabel lbGuide;
+	private JButton btEmployee;
+	private JButton btEmployment;
+	private JButton btFSector;
+	private JButton btExit;
+	
+	
+	public MainScreenI() {
+		super("Welcome!");
+		config();
+	}
+	
+	private void config() {
+		Container container = getContentPane();
+		container.setLayout(new GridBagLayout());
+		GridBagConstraints cons = new GridBagConstraints();
+	}
 }
